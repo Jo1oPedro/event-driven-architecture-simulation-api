cd314e7a2b4434bd0c0a4c24ca016e05ca1be411
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-04-04 14:08:51 -0300

    add index method on topologyController

--- src/Controller/TopologyController.php
+++ src/Controller/TopologyController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
+use App\DTO\Response\TopologyListResponse;
+use App\Service\TopologyService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TopologyController extends AbstractController
 {
-    #[Route('/topology', name: 'app_topology')]
+    public function __construct(
+        private readonly TopologyService $topologyService
+    ) {}
+
+    #[Route('/', methods: ['GET'], name: 'index_topology')]
     public function index(): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TopologyController.php',
-        ]);
+        $topologies = $this->topologyService->findAll();
+
+        $response = array_map(
+            function($topology) {
+                new TopologyListResponse($topology);
+            },
+            $topologies
+        );
+
+        return new JsonResponse($response);
     }
 }
