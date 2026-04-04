5bbf7af421ba4862c9d73481856e6e4b588c7992
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-04-04 16:21:22 -0300

    add reflection-docblock, property acess and serializar for map request payload to work

--- src/Controller/TopologyController.php
+++ src/Controller/TopologyController.php
@@ -8,29 +8,28 @@ use App\DTO\Response\TopologyResponse;
 use App\Service\TopologyService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/topologies')]
 final class TopologyController extends AbstractController
 {
     public function __construct(
-        private readonly TopologyService $topologyService
+        private readonly TopologyService $topologyService,
     ) {}
 
-    #[Route('/', methods: ['GET'], name: 'index_topology')]
+    #[Route('', methods: ['GET'], name: 'index_topology')]
     public function index(): JsonResponse
     {
         $topologies = $this->topologyService->findAll();
 
         $response = array_map(
-            function($topology) {
-                new TopologyListResponse($topology);
-            },
+            fn($topology) => new TopologyListResponse($topology),
             $topologies
         );
 
-        return new JsonResponse($response);
+        return $this->json($response);
     }
 
     #[Route('/{id}', methods: ['GET'], name: 'show_topology')]
@@ -38,51 +37,46 @@ final class TopologyController extends AbstractController
     {
         $topology = $this->topologyService->findById($id);
 
-        if(!$topology) {
-            return $this->json(['error' => 'Typology not found'], Response::HTTP_NOT_FOUND);
+        if (!$topology) {
+            return $this->json(['error' => 'Topology not found'], Response::HTTP_NOT_FOUND);
         }
 
         return $this->json(new TopologyResponse($topology));
     }
 
     #[Route('', methods: ['POST'], name: 'create_topology')]
-    public function create(Request $request): JsonResponse
+    public function create(#[MapRequestPayload] SaveTopologyRequest $request): JsonResponse
     {
-        $data = json_encode($request->getContent(), true);
+        $topology = $this->topologyService->create($request);
 
-        $topologyRequest = new SaveTopologyRequest(
-            name: $data['name'],
-            description: $data['description'] ?? null,
-            nodes: $data['nodes'] ?? [],
-            edges: $data['edges'] ?? [],
-        );
-
-        $topology = $this->topologyService->create($topologyRequest);
-
-        return $this->json(
-            new TopologyResponse($topology),
-            Response::HTTP_CREATED
-        );
+        return $this->json(new TopologyResponse($topology), Response::HTTP_CREATED);
     }
 
     #[Route('/{id}', methods: ['PUT'], name: 'update_topology')]
-    public function update(int $id, Request $request): JsonResponse
+    public function update(int $id, #[MapRequestPayload] SaveTopologyRequest $request): JsonResponse
     {
         $topology = $this->topologyService->findById($id);
 
-        if(!$topology) {
+        if (!$topology) {
             return $this->json(['error' => 'Topology not found'], Response::HTTP_NOT_FOUND);
         }
 
-        $topologyRequest = new SaveTopologyRequest(
-            name: $topology['name'],
-            description: $topology['description'] ?? null,
-            nodes: $topology['nodes'] ?? [],
-            edges: $topology['edges'] ?? [],
-        );
-
-        $topology = $this->topologyService->update($topology, $topologyRequest);
+        $topology = $this->topologyService->update($topology, $request);
 
         return $this->json(new TopologyResponse($topology));
+    }
+
+    #[Route('/{id}', methods: ['DELETE'], name: 'delete_topology')]
+    public function delete(int $id): JsonResponse
+    {
+        $topology = $this->topologyService->findById($id);
+
+        if (!$topology) {
+            return $this->json(['error' => 'Topology not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $this->topologyService->delete($topology);
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
     }
 }
